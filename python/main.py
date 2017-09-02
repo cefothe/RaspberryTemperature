8fc8686ca5018ef4a9c2eca8e40a82a53d673e60
Gabriela Georgieva <gabrielageorgieva9412@gmail.com>
2017-09-02 19:24:35 +0000

    add python code

---
+++ python/main.py
@@ -0,0 +1,62 @@
+import RPi.GPIO as GPIO
+import dht11
+import time
+import lcdui
+
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+GPIO.setup(37, GPIO.OUT) #RED diode
+GPIO.setup(35, GPIO.OUT) #WHITE diode
+
+GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_UP) #plus
+GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP) #minus
+
+temperature = 25 #Default temperature is 16 
+
+# read data using pin 7
+instance =dht11.DHT11(pin=7)
+initFlag = False
+debug = False
+backlight = True
+
+device = lcdui.lcd(0x3F, 1, backlight, initFlag)
+print("Start program")
+
+startTemp = '';
+
+result=instance.read()
+if result.is_valid():
+      startTemp= int(result.temperature)
+      
+
+#main loop
+while True:
+   button_one = GPIO.input(33)
+   if button_one == False:
+        temperature = temperature + 1
+        print("You increase temperature to", temperature)
+   button_two = GPIO.input(31)
+   if button_two == False:
+        temperature = temperature - 1
+        print("You reduces temperature to", temperature)
+
+   #slight pause to debounce
+   time.sleep(0.05)   
+
+   result=instance.read()
+   if result.is_valid():
+      temp = result.temperature
+      if startTemp != int(temp):
+         device.lcd_puts(str(temp), 1)
+      if temp > temperature: 
+         GPIO.output(37, True)
+         GPIO.output(35, False)
+      else:
+         GPIO.output(37, False)
+         GPIO.output(35, True)
+   
+    
+
